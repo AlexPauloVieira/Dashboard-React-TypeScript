eade01aee8f2036cd365cfd668e3e135076cec93
Alex Vieira <alex.vieira30@etec.sp.gov.br>
2020-12-27 21:29:11 -0300

    Creating main header

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,11 +1,21 @@
-import React from "react";
-
-import { Container } from "./styles";
+import React, { useMemo } from "react";
+import emojis from "../../utils/emojis";
+import { Container, Profile, Welcome, UserName } from "./styles";
 
 const MainHeader: React.FC = () => {
+  const emoji = useMemo(() => {
+    const indice = Math.floor(Math.random() * emojis.length);
+    return emojis[indice];
+  }, []);
+
   return (
     <Container>
-      <h1>MainHeader</h1>
+      <h1>Toggle</h1>
+
+      <Profile>
+        <Welcome>Olá, {emoji}</Welcome>
+        <UserName>Alex Paulo</UserName>
+      </Profile>
     </Container>
   );
 };
