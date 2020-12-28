4477a3d948af79f8c4961dae96160776063d8b01
Alex Vieira <alex.vieira30@etec.sp.gov.br>
2020-12-28 18:33:55 -0300

    Select component and dynamic header

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,13 +1,22 @@
 import React from "react";
 
 import ContentHeader from "../../components/ContentHeader";
+import SelectInput from "../../components/SelectInput";
 
 import { Container } from "./styles";
 
 const Dashboard: React.FC = () => {
+  const options = [
+    { value: "Alex", label: "Alex" },
+    { value: "Maria", label: "Maria" },
+    { value: "Bob", label: "Bob" },
+  ];
+
   return (
     <Container>
-      <ContentHeader />
+      <ContentHeader title='Dashboard' lineColor='#ffcc00'>
+        <SelectInput options={options} />
+      </ContentHeader>
     </Container>
   );
 };
--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,7 +1,24 @@
 import React from "react";
 
+import ContentHeader from "../../components/ContentHeader";
+import SelectInput from "../../components/SelectInput";
+
+import { Container } from "./styles";
+
 const List: React.FC = () => {
-  return <h1>List</h1>;
+  const options = [
+    { value: "Alex", label: "Alex" },
+    { value: "Maria", label: "Maria" },
+    { value: "Bob", label: "Bob" },
+  ];
+
+  return (
+    <Container>
+      <ContentHeader title='List' lineColor='#ffcc00'>
+        <SelectInput options={options} />
+      </ContentHeader>
+    </Container>
+  );
 };
 
 export default List;
