78191dc1b862cf26c879319c3afcb4c86a42b32c
Alex Vieira <alex.vieira30@etec.sp.gov.br>
2021-01-04 17:38:09 -0300

    Creating pie chart

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import logoImg from "../../assets/logo.svg";
 
 import {
--- src/components/ContentHeader/index.tsx
+++ src/components/ContentHeader/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-import SelectInput from "../../components/SelectInput";
 import { Container, TitleContainer, Controllers } from "./styles";
 
 interface IContentHeaderProps {
