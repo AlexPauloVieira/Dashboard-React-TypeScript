11a30ae3c04c108aa3699f1aaa4c4887f7e0a2a7
Alex Vieira <alex.vieira30@etec.sp.gov.br>
2021-01-05 18:49:38 -0300

    History Chart not working, but almost done

--- src/components/HistoryBox/styles.ts
+++ src/components/HistoryBox/styles.ts
@@ -5,46 +5,40 @@ interface ILegendProps {
 }
 
 export const Container = styled.div`
-  width: 48%;
-  height: 260px;
+  width: 100%;
 
-  margin: 10px 0;
+  display: flex;
+  flex-direction: column;
 
   background-color: ${(props) => props.theme.colors.tertiary};
   color: ${(props) => props.theme.colors.white};
 
-  border-radius: 7px;
+  margin: 10px 0;
+  padding: 30px 20px;
 
-  display: flex;
+  border-radius: 7px;
 `;
 
-export const SideLeft = styled.aside`
-  padding: 30px 20px;
+export const ChartContainer = styled.div`
+  flex: 1;
+  height: 260px;
+`;
+
+export const Header = styled.header`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
 
   > h2 {
     margin-bottom: 20px;
+    padding-left: 16px;
   }
 `;
 
 export const LegendContainer = styled.ul`
+  display: flex;
+  padding-right: 16px;
   list-style: none;
-
-  height: 175px;
-  padding-right: 15px;
-  overflow-y: scroll;
-
-  ::-webkit-scrollbar {
-    width: 10px;
-  }
-
-  ::-webkit-scrollbar-thumb {
-    background-color: ${(props) => props.theme.colors.secondary};
-    border-radius: 10px;
-  }
-
-  ::-webkit-scrollbar-track {
-    background-color: ${(props) => props.theme.colors.tertiary};
-  }
 `;
 
 export const Legend = styled.li<ILegendProps>`
@@ -52,6 +46,7 @@ export const Legend = styled.li<ILegendProps>`
   align-items: center;
 
   margin-bottom: 7px;
+  margin-left: 7px;
 
   > div {
     background-color: ${(props) => props.color};
@@ -60,7 +55,7 @@ export const Legend = styled.li<ILegendProps>`
     height: 40px;
     border-radius: 5px;
 
-    font-size: 18px;
+    font-size: 14px;
     line-height: 40px;
     text-align: center;
   }
@@ -69,5 +64,3 @@ export const Legend = styled.li<ILegendProps>`
     margin-left: 5px;
   }
 `;
-
-export const SideRight = styled.main``;
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -4,7 +4,8 @@ import ContentHeader from "../../components/ContentHeader";
 import SelectInput from "../../components/SelectInput";
 import WalletBox from "../../components/WalletBox";
 import MessageBox from "../../components/MessageBox";
-import PieChart from "../../components/PieChart";
+import PieChartBox from "../../components/PieChartBox";
+import HistoryBox from "../../components/HistoryBox";
 
 import gains from "../../repositories/gains";
 import expenses from "../../repositories/expenses";
@@ -117,6 +118,85 @@ const Dashboard: React.FC = () => {
     }
   }, [totalGains, totalExpenses]);
 
+  const relationExpensesVersusGains = useMemo(() => {
+    const total = totalGains + totalExpenses;
+
+    const percentGains = (totalGains / total) * 100;
+    const percentExpenses = (totalExpenses / total) * 100;
+
+    const data = [
+      {
+        name: "Entradas",
+        value: totalGains,
+        percent: Number(percentGains.toFixed(1)),
+        color: "#F7931B",
+      },
+      {
+        name: "Saídas",
+        value: totalExpenses,
+        percent: Number(percentExpenses.toFixed(1)),
+        color: "#E44C4E",
+      },
+    ];
+
+    return data;
+  }, [totalGains, totalExpenses]);
+
+  const historyData = useMemo(() => {
+    return listOfMonths
+      .map((_, month) => {
+        let amountEntry = 0;
+        gains.forEach((gain) => {
+          const date = new Date(gain.date);
+          const gainMonth = date.getMonth();
+          const gainYear = date.getFullYear();
+
+          if (gainMonth === month && gainYear === yearSelected) {
+            try {
+              amountEntry += Number(gain.amount);
+            } catch {
+              throw new Error(
+                "Amount entry is invalid, amountEntry must be a number"
+              );
+            }
+          }
+        });
+
+        let amountOutput = 0;
+        expenses.forEach((expense) => {
+          const date = new Date(expense.date);
+          const expenseMonth = date.getMonth();
+          const expenseYear = date.getFullYear();
+
+          if (expenseMonth === month && expenseYear === yearSelected) {
+            try {
+              amountOutput += Number(expense.amount);
+            } catch {
+              throw new Error(
+                "Amount output is invalid, amountOutput must be a number"
+              );
+            }
+          }
+        });
+
+        return {
+          monthNumber: month,
+          month: listOfMonths[month].substr(0, 3),
+          amountEntry,
+          amountOutput,
+        };
+      })
+      .filter((item) => {
+        const currentMonth = new Date().getMonth();
+        const currentYear = new Date().getFullYear();
+
+        return (
+          (yearSelected === currentYear && item.monthNumber <= currentMonth) ||
+          yearSelected < currentYear
+        );
+      });
+  }, [yearSelected]);
+
   const handleMonthSelected = (month: string) => {
     try {
       const parsedMonth = Number(month);
@@ -179,7 +259,13 @@ const Dashboard: React.FC = () => {
           icon={message.icon}
         />
 
-        <PieChart />
+        <PieChartBox data={relationExpensesVersusGains} />
+
+        <HistoryBox
+          data={historyData}
+          lineColorAmountEntry='#F7931B'
+          lineColorAmountOutput='#E44C4E'
+        />
       </Content>
     </Container>
   );
