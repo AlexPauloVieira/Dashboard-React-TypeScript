ecb6042721f2ee6d45331d2319b71af2d14af23f
Alex Vieira <alex.vieira30@etec.sp.gov.br>
2021-01-08 20:43:46 -0300

    Login Page

--- src/pages/Singin/index.tsx
+++ src/pages/Singin/index.tsx
@@ -1,7 +1,27 @@
 import React from "react";
 
+import logoImg from "../../assets/logo.svg";
+import Button from "../../components/Button";
+import Input from "../../components/Input";
+
+import { Container, Logo, Form, FormTitle } from "./styles";
+
 const SignIn: React.FC = () => {
-  return <h1>SignIn</h1>;
+  return (
+    <Container>
+      <Logo>
+        <img src={logoImg} alt='My Wallet' />
+        <h2>My Wallet</h2>
+      </Logo>
+      <Form onSubmit={() => {}}>
+        <FormTitle>Entrar</FormTitle>
+        <Input type='email' placeholder='E-mail' required />
+        <Input type='password' placeholder='Senha' required />
+
+        <Button type='submit'>Acessar</Button>
+      </Form>
+    </Container>
+  );
 };
 
 export default SignIn;
